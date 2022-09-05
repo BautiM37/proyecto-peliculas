88dc5f1406bd6661ae57be74ee18e2ff0459ad38
BautiM37 <bmigliore@udesa.edu.ar>
2022-09-05 09:42:26 -0300

    creacion componentes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-      </Header>
+        <Header />
+      </header>
     </div>
   );
 }
---
+++ src/components/CadaPeliculaPopular/CadaPeliculaPopular.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import './cadaPeliculaPopular.css';
+
+class CadaPeliculaPopular extends Component{
+    constructor(){
+        super()
+        this.state={
+            
+        }
+    }
+}
