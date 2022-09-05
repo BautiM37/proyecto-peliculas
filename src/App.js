7e9e8350b8d7d26f5ab4f0b6d7bc0d0f70c5bf2a
BautiM37 <bmigliore@udesa.edu.ar>
2022-09-05 11:25:13 -0300

    fetch peliculas cartelera

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
+import PeliculasCartel from './components/PeliculasCartel/PeliculasCartel';
 import PeliculasPopulares from './components/PeliculasPopulares/PeliculasPopulares';
 
 function App() {
@@ -7,7 +8,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Header/>
+        <h2>Peliculas Populares</h2>
         <PeliculasPopulares/>
+        <h2>Peliculas en Cartel</h2>
+        <PeliculasCartel/>
       </header>
     </div>
   );
---
+++ src/components/PeliculasCartel/PeliculasCartel.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import './peliculasCartel.css'
+import CadaPelicula from "../CadaPelicula/CadaPelicula";
+
+class PeliculasCartel extends Component{
+    constructor(){
+        super()
+        this.state={
+            peliculas:[]
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=51c5421e7c7f38a93e388ad6d2405b1f&language=en-US&page=1')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    peliculas: data.results
+
+                });
+            })
+            .catch()
+    }
+
+    render(){
+        return(
+            <div>
+                <section>
+                    {
+                        this.state.peliculas.map((unaPelicula, idx) => <CadaPelicula key={unaPelicula.title+idx}
+                        pelicula={unaPelicula}/>)
+                        
+                    }
+                </section>
+            </div>
+        )
+    }
+}   
+
+export default PeliculasCartel;
