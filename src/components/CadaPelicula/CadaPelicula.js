8ff2aafeeae764c6e9d4362362bf949cc5547385
BautiM37 <bmigliore@udesa.edu.ar>
2022-09-08 19:12:52 -0300

    comienzo de favoritos

--- src/components/CadaPelicula/CadaPelicula.js
+++ src/components/CadaPelicula/CadaPelicula.js
@@ -8,7 +8,25 @@ class CadaPelicula extends Component {
             verMas: false,
             claseVerMas: 'esconder',
             textoVerMas: 'Ver Más',
-            infoHover: 'hide'
+            infoHover: 'hide',
+            esFavorito: false,
+            iconoEstrella: "fas fa-regular fa-star"
+        }
+    }
+
+    componentDidMount() {
+        let favoritos = [];
+        let conseguirFavoritos = localStorage.getItem('favoritos');
+
+        if (conseguirFavoritos !== null) {
+            let favsArray = JSON.parse(conseguirFavoritos);
+            favoritos = favsArray
+
+            if (favoritos.includes(this.props.pelicula.id)) {
+                this.setState({
+                    iconoEstrella: "fas fa-solid fa-star"
+                })
+            }
         }
     }
 
@@ -28,6 +46,32 @@ class CadaPelicula extends Component {
         }
     }
 
+    agregarYSacarFavs(id) {
+        let favoritos = [];
+        let conseguirFavoritos = localStorage.getItem('favoritos');
+
+        if (conseguirFavoritos !== null) {
+            let favsArray = JSON.parse(conseguirFavoritos);
+            favoritos = favsArray
+        }
+        if (favoritos.includes(id)){
+            favoritos = favoritos.filter( cadaID => cadaID !== id);
+            this.setState({
+                iconoEstrella: "fas fa-regular fa-star"
+            })
+        } else {
+            favoritos.push(id);
+            this.setState({
+                iconoEstrella: "fas fa-solid fa-star"
+            })
+        }
+
+        let favsString = JSON.stringify(favoritos);
+        localStorage.setItem('favoritos', favsString)
+
+        console.log(localStorage)
+    }
+
     render() {
         return (
             <article
@@ -38,6 +82,7 @@ class CadaPelicula extends Component {
                 <section className={'seccion-' + this.state.infoHover}>
                     <h2 className='titulo'>{this.props.pelicula.title}</h2>
                     <button className='boton-ver-mas' onClick={() => this.verMasVerMenos()}>{this.state.textoVerMas}</button>
+                    <i className={this.state.iconoEstrella}></i>
                     <div className={'info-extra-' + this.state.claseVerMas}>
                         {this.props.pelicula.overview.length > 250 ? 
                             <p className='overview-pelicula'>{this.props.pelicula.overview.slice(0, 250)}...</p>
