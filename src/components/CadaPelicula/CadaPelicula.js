4725c657b7ff1e2a8d01e8e3f982cd3bd20360ba
BautiM37 <bmigliore@udesa.edu.ar>
2022-09-12 10:28:08 -0300

    favoritos sin css

--- src/components/CadaPelicula/CadaPelicula.js
+++ src/components/CadaPelicula/CadaPelicula.js
@@ -10,21 +10,23 @@ class CadaPelicula extends Component {
             textoVerMas: 'Ver Más',
             infoHover: 'hide',
             esFavorito: false,
-            iconoEstrella: "fas fa-regular fa-star"
+            iconoEstrella: "fas fa-regular fa-circle icono-estrella"
         }
     }
 
+    // con el DidMount chequeamos el estado de las tarjetas para evitar que, al recargarse la página, estas se renderizen por primera vez y muestren un estado incorrecto (favoritos). Todo esto maneja que el ícono de estrella de fav sea correcto.
     componentDidMount() {
         let favoritos = [];
-        let conseguirFavoritos = localStorage.getItem('favoritos');
+        let conseguirFavoritos = localStorage.getItem('favoritos'); // puede estar vacío
 
         if (conseguirFavoritos !== null) {
+            // para trabajar bien con el localStorage, tenemos que pasar la info a array. Va a ser muy dificil trabajar con una cadena de texto.
             let favsArray = JSON.parse(conseguirFavoritos);
             favoritos = favsArray
 
-            if (favoritos.includes(this.props.pelicula.id)) {
+            if (favoritos.includes(this.props.pelicula.id)) { // includes --> true o false
                 this.setState({
-                    iconoEstrella: "fas fa-solid fa-star"
+                    iconoEstrella: "fas fa-solid fa-star icono-estrella"
                 })
             }
         }
@@ -54,18 +56,21 @@ class CadaPelicula extends Component {
             let favsArray = JSON.parse(conseguirFavoritos);
             favoritos = favsArray
         }
-        if (favoritos.includes(id)){
-            favoritos = favoritos.filter( cadaID => cadaID !== id);
+        if (favoritos.includes(id)) {
+            // sacar el ID del array, pisando el array de favoritos con el filtro nuevo
+            favoritos = favoritos.filter(cadaID => cadaID !== id);
             this.setState({
-                iconoEstrella: "fas fa-regular fa-star"
+                iconoEstrella: "fas fa-regular fa-circle icono-estrella"
             })
         } else {
+            // cuando el ID no está en el array, lo agregamos
             favoritos.push(id);
             this.setState({
-                iconoEstrella: "fas fa-solid fa-star"
+                iconoEstrella: "fas fa-solid fa-star icono-estrella"
             })
         }
 
+        // convertir a string para almacenarlo en localStorage
         let favsString = JSON.stringify(favoritos);
         localStorage.setItem('favoritos', favsString)
 
@@ -75,19 +80,22 @@ class CadaPelicula extends Component {
     render() {
         return (
             <article
-                onMouseEnter={() => this.setState({infoHover: 'show'})}
-                onMouseLeave={() => this.setState({infoHover: 'hide'})}
+                onMouseEnter={() => this.setState({ infoHover: 'show' })}
+                onMouseLeave={() => this.setState({ infoHover: 'hide' })}
                 className='tarjeta-pelicula' >
                 <img src={'https://image.tmdb.org/t/p/w342/' + this.props.pelicula.poster_path} alt="" className='imagen-pelicula' />
+
+                {/* Como estamos en la misma clase que el método, el arrow function no necesita al id como parámetro */}
+                <i onClick={() => this.agregarYSacarFavs(this.props.pelicula.id)} className={this.state.iconoEstrella}></i>
+
                 <section className={'seccion-' + this.state.infoHover}>
                     <h2 className='titulo'>{this.props.pelicula.title}</h2>
                     <button className='boton-ver-mas' onClick={() => this.verMasVerMenos()}>{this.state.textoVerMas}</button>
-                    <i className={this.state.iconoEstrella}></i>
                     <div className={'info-extra-' + this.state.claseVerMas}>
-                        {this.props.pelicula.overview.length > 250 ? 
+                        {this.props.pelicula.overview.length > 250 ?
                             <p className='overview-pelicula'>{this.props.pelicula.overview.slice(0, 250)}...</p>
-                        :
-                        <p className='overview-pelicula'>{this.props.pelicula.overview}</p>
+                            :
+                            <p className='overview-pelicula'>{this.props.pelicula.overview}</p>
                         }
                     </div>
                 </section>
@@ -98,4 +106,4 @@ class CadaPelicula extends Component {
 
 }
 
-export default CadaPelicula
+export default CadaPelicula;
---
+++ src/screens/Favoritos/Favoritos.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import CadaPelicula from "../../components/CadaPelicula/CadaPelicula";
+
+class Favoritos extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            peliculas: [],
+            hayFavs: false
+        }
+    }
+
+    componentDidMount() {
+        let conseguirFavoritos = localStorage.getItem('favoritos');
+        let favoritos = [];
+
+        if (conseguirFavoritos !== null) {
+            let favsArray = JSON.parse(conseguirFavoritos);
+            favsArray.map(unFavorito => fetch("https://api.themoviedb.org/3/movie/" + unFavorito + "?api_key=51c5421e7c7f38a93e388ad6d2405b1f&language=en-US")
+            .then(res => res.json())
+            .then(data => {
+                favoritos.push(data)
+                this.setState({
+                    peliculas: favoritos,
+                    hayFavs: true
+                });
+            })
+            .catch())
+        } else {
+            this.setState({
+                hayFavs: false
+            })
+        }
+    }
+
+    render() {
+        return (
+            <section>
+                <h2>Favoritos</h2>
+
+                {
+                    this.state.peliculas.map((unaPeli, idx) =>
+                        <CadaPelicula key={unaPeli.title + idx} pelicula={unaPeli} />
+                    )}
+            </section>
+        )
+    }
+}
+
+export default Favoritos;
